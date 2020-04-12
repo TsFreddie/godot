d94cfb457ec7456b89ebe987184b39e6ea07e027
Freddie <tsfreddiewang@gmail.com>
2020-04-12 21:10:50 +0100

    clang-format

--- modules/ram_audio/audio_stream_ram.cpp
+++ modules/ram_audio/audio_stream_ram.cpp
@@ -1,111 +1,111 @@
 
 #include "audio_stream_ram.h"
-#include "thirdparty/misc/stb_vorbis.c"
 #include "core/os/file_access.h"
+#include "thirdparty/misc/stb_vorbis.c"
 
-AudioStreamRAM::AudioStreamRAM()
-        : capacity(SAFE_FRAMES), nframes(0), length(0) {
-    mix_rate = AudioServer::get_singleton()->get_mix_rate();
-    data = (AudioFrame *)memalloc(capacity * sizeof(AudioFrame));
-    valid = data != NULL;
+AudioStreamRAM::AudioStreamRAM() :
+		capacity(SAFE_FRAMES), nframes(0), length(0) {
+	mix_rate = AudioServer::get_singleton()->get_mix_rate();
+	data = (AudioFrame *)memalloc(capacity * sizeof(AudioFrame));
+	valid = data != NULL;
 }
 
 AudioStreamRAM::~AudioStreamRAM() {
-    if (data != NULL) memfree(data);
-    data = NULL;
+	if (data != NULL) memfree(data);
+	data = NULL;
 }
 
 int AudioStreamRAM::_decode_vorbis(String filename) {
-    if (data == NULL) return -2;
+	if (data == NULL) return -2;
 
-    Error r_error;
-    int error;
+	Error r_error;
+	int error;
 
-    FileAccess *file = FileAccess::open(filename, FileAccess::READ, &r_error);
-    size_t len = file->get_len();
-    uint8_t *file_buf = (uint8_t *)memalloc(len * sizeof(uint8_t));
-    file->get_buffer(file_buf, len);
-    file->close();
+	FileAccess *file = FileAccess::open(filename, FileAccess::READ, &r_error);
+	size_t len = file->get_len();
+	uint8_t *file_buf = (uint8_t *)memalloc(len * sizeof(uint8_t));
+	file->get_buffer(file_buf, len);
+	file->close();
 
-    stb_vorbis *f = stb_vorbis_open_memory(file_buf, len, &error, NULL);
-    if (f == NULL) return -1;
+	stb_vorbis *f = stb_vorbis_open_memory(file_buf, len, &error, NULL);
+	if (f == NULL) return -1;
 
-    int sample_rate = f->sample_rate;
-    int channels = f->channels;
+	int sample_rate = f->sample_rate;
+	int channels = f->channels;
 
-    float **buffer = NULL;
+	float **buffer = NULL;
 
-    for (;;) {
-        float nsamples = stb_vorbis_get_frame_float(f, NULL, &buffer);
+	for (;;) {
+		float nsamples = stb_vorbis_get_frame_float(f, NULL, &buffer);
 
-        if (nsamples <= 0) break;
+		if (nsamples <= 0) break;
 
-        while (capacity < nframes + nsamples) {
-            capacity *= 2;
-            AudioFrame *new_data = (AudioFrame *)memrealloc(data, capacity * sizeof(AudioFrame));
-            if (new_data == NULL) {
-                stb_vorbis_close(f);
-                memfree(file_buf);
-                memfree(data);
-                data = NULL;
-                valid = false;
-                return -2;
-            }
-            data = new_data;
-        }
+		while (capacity < nframes + nsamples) {
+			capacity *= 2;
+			AudioFrame *new_data = (AudioFrame *)memrealloc(data, capacity * sizeof(AudioFrame));
+			if (new_data == NULL) {
+				stb_vorbis_close(f);
+				memfree(file_buf);
+				memfree(data);
+				data = NULL;
+				valid = false;
+				return -2;
+			}
+			data = new_data;
+		}
 
-        for (uint32_t i = 0; i < nsamples; ++i) {
-            if (channels >= 2) {
-                data[nframes + i] = AudioFrame(buffer[0][i], buffer[1][i]);
-            } else {
-                data[nframes + i] = AudioFrame(buffer[0][i], buffer[0][i]);
-            }
-        }
-        
-        nframes += nsamples;
-    }
+		for (uint32_t i = 0; i < nsamples; ++i) {
+			if (channels >= 2) {
+				data[nframes + i] = AudioFrame(buffer[0][i], buffer[1][i]);
+			} else {
+				data[nframes + i] = AudioFrame(buffer[0][i], buffer[0][i]);
+			}
+		}
 
-    memfree(file_buf);
+		nframes += nsamples;
+	}
 
-    if (sample_rate != mix_rate) {
-        stb_vorbis_close(f);
-        return _resample_from(sample_rate);
-    }
-    
-    if (capacity > nframes) {
-        AudioFrame *fit_data = (AudioFrame *)memrealloc(data, nframes * sizeof(AudioFrame));
-        if (fit_data == NULL) {
-            memfree(data);
-            data = NULL;
-            valid = false;
-            return -2;
-        }
-        data = fit_data;
-        capacity = nframes;
-    }
+	memfree(file_buf);
 
-    stb_vorbis_close(f);
-    return nframes;
+	if (sample_rate != mix_rate) {
+		stb_vorbis_close(f);
+		return _resample_from(sample_rate);
+	}
+
+	if (capacity > nframes) {
+		AudioFrame *fit_data = (AudioFrame *)memrealloc(data, nframes * sizeof(AudioFrame));
+		if (fit_data == NULL) {
+			memfree(data);
+			data = NULL;
+			valid = false;
+			return -2;
+		}
+		data = fit_data;
+		capacity = nframes;
+	}
+
+	stb_vorbis_close(f);
+	return nframes;
 }
 
 int AudioStreamRAM::_resample_from(int source_rate) {
-    if (source_rate == 0) return -1;
-    if (data == NULL) {
-        return -1;
-    }
+	if (source_rate == 0) return -1;
+	if (data == NULL) {
+		return -1;
+	}
 
-    uint32_t new_length = uint32_t(nframes * (mix_rate / double(source_rate)));
-    AudioFrame *new_data = (AudioFrame *)memalloc((new_length) * sizeof(AudioFrame));
-    if (new_data == NULL) {
-        free(data);
-        data = NULL;
-        valid = false;
-        return -2;
-    }
+	uint32_t new_length = uint32_t(nframes * (mix_rate / double(source_rate)));
+	AudioFrame *new_data = (AudioFrame *)memalloc((new_length) * sizeof(AudioFrame));
+	if (new_data == NULL) {
+		free(data);
+		data = NULL;
+		valid = false;
+		return -2;
+	}
 
-    uint64_t mix_offset = 0;
+	uint64_t mix_offset = 0;
 
-    uint64_t mix_increment = uint64_t((source_rate / double(mix_rate)) * double(FP_LEN));
+	uint64_t mix_increment = uint64_t((source_rate / double(mix_rate)) * double(FP_LEN));
 
 	for (int i = 0; i < new_length; ++i) {
 		uint32_t idx = 4 + uint32_t(mix_offset >> FP_BITS);
@@ -126,26 +126,26 @@ int AudioStreamRAM::_resample_from(int source_rate) {
 		mix_offset += mix_increment;
 	}
 
-    memfree(data);
-    data = new_data;
-    nframes = capacity = new_length;
-    return nframes;
+	memfree(data);
+	data = new_data;
+	nframes = capacity = new_length;
+	return nframes;
 }
 
 void AudioStreamRAM::update_length() {
-    length = (nframes / (float)mix_rate);
+	length = (nframes / (float)mix_rate);
 }
 
 void AudioStreamRAM::load(String path) {
-    if (path.ends_with(".ogg")) {
-        _decode_vorbis(path);
-    }
+	if (path.ends_with(".ogg")) {
+		_decode_vorbis(path);
+	}
 
-    update_length();
+	update_length();
 }
 
 bool AudioStreamRAM::is_valid() {
-    return valid;
+	return valid;
 }
 
 // uint32_t AudioStreamRAM::get_frame_count() {
@@ -153,92 +153,92 @@ bool AudioStreamRAM::is_valid() {
 // }
 
 Ref<AudioStreamPlayback> AudioStreamRAM::instance_playback() {
-    Ref<AudioStreamPlaybackRAM> playback;
-    playback.instance();
-    playback->base = Ref<AudioStreamRAM>(this);
-    return playback;
+	Ref<AudioStreamPlaybackRAM> playback;
+	playback.instance();
+	playback->base = Ref<AudioStreamRAM>(this);
+	return playback;
 }
 
 String AudioStreamRAM::get_stream_name() const {
-    return "RAMAudio";
+	return "RAMAudio";
 }
 
 float AudioStreamRAM::get_length() const {
-    return length;
+	return length;
 }
 
 void AudioStreamRAM::_bind_methods() {
-    ClassDB::bind_method("load", &AudioStreamRAM::load);
-    ClassDB::bind_method("is_valid", &AudioStreamRAM::is_valid);
-    // ClassDB::bind_method("get_frame_count", &AudioStreamRAM::get_frame_count);
+	ClassDB::bind_method("load", &AudioStreamRAM::load);
+	ClassDB::bind_method("is_valid", &AudioStreamRAM::is_valid);
+	// ClassDB::bind_method("get_frame_count", &AudioStreamRAM::get_frame_count);
 }
 
-AudioStreamPlaybackRAM::AudioStreamPlaybackRAM()
-        : active(false), position(0) {
+AudioStreamPlaybackRAM::AudioStreamPlaybackRAM() :
+		active(false), position(0) {
 }
 
 AudioStreamPlaybackRAM::~AudioStreamPlaybackRAM() {
 }
 
 void AudioStreamPlaybackRAM::stop() {
-    active = false;
+	active = false;
 }
 
 void AudioStreamPlaybackRAM::start(float p_from_pos) {
-    ERR_FAIL_COND(!(base->valid));
-    if (!(base->valid)) return;
+	ERR_FAIL_COND(!(base->valid));
+	if (!(base->valid)) return;
 
-    seek(p_from_pos);
-    active = true;
+	seek(p_from_pos);
+	active = true;
 }
 
 void AudioStreamPlaybackRAM::seek(float p_time) {
-    ERR_FAIL_COND(!(base->valid));
-    if (!(base->valid)) return;
+	ERR_FAIL_COND(!(base->valid));
+	if (!(base->valid)) return;
 
-    uint32_t max = base->nframes;
-    position = p_time * base->mix_rate;
-    if (position < 0 || position >= max) {
-        position = 0;
-    }
+	uint32_t max = base->nframes;
+	position = p_time * base->mix_rate;
+	if (position < 0 || position >= max) {
+		position = 0;
+	}
 }
 
 void AudioStreamPlaybackRAM::mix(AudioFrame *p_buffer, float p_rate, int p_frames) {
-    ERR_FAIL_COND(!active);
-    if (!active) {
-        return;
-    }
+	ERR_FAIL_COND(!active);
+	if (!active) {
+		return;
+	}
 
-    uint32_t max = base->nframes;
-    uint32_t end_of_mix = position + p_frames;
-    int mix_frames = p_frames;
+	uint32_t max = base->nframes;
+	uint32_t end_of_mix = position + p_frames;
+	int mix_frames = p_frames;
 
-    if (max <= end_of_mix) {
-        mix_frames = p_frames - (end_of_mix - max);
-        end_of_mix = max;
-        active = false;
-    }
+	if (max <= end_of_mix) {
+		mix_frames = p_frames - (end_of_mix - max);
+		end_of_mix = max;
+		active = false;
+	}
 
-    for(int i = 0; i < mix_frames; ++i) {
-        p_buffer[i] = base->data[position + i];
-    }
+	for (int i = 0; i < mix_frames; ++i) {
+		p_buffer[i] = base->data[position + i];
+	}
 
-    for(int i = mix_frames; i < p_frames; ++i) {
-        p_buffer[i] = AudioFrame(0, 0);
-    }
+	for (int i = mix_frames; i < p_frames; ++i) {
+		p_buffer[i] = AudioFrame(0, 0);
+	}
 
-    position = end_of_mix;
+	position = end_of_mix;
 }
 
 int AudioStreamPlaybackRAM::get_loop_count() const {
-    return 0;
+	return 0;
 }
 float AudioStreamPlaybackRAM::get_playback_position() const {
-    return position / (float)base->mix_rate;
+	return position / (float)base->mix_rate;
 }
 float AudioStreamPlaybackRAM::get_length() const {
-    return base->length;
+	return base->length;
 }
 bool AudioStreamPlaybackRAM::is_playing() const {
-    return active;
+	return active;
 }
--- modules/ram_audio/audio_stream_ram.h
+++ modules/ram_audio/audio_stream_ram.h
@@ -1,70 +1,70 @@
 #ifndef AUDIO_STREAM_RAM_H
 #define AUDIO_STREAM_RAM_H
 
+#include "core/math/audio_frame.h"
 #include "core/reference.h"
 #include "core/resource.h"
 #include "servers/audio/audio_stream.h"
-#include "core/math/audio_frame.h"
 
-class AudioStreamRAM: public AudioStream {
-    GDCLASS(AudioStreamRAM, AudioStream)
+class AudioStreamRAM : public AudioStream {
+	GDCLASS(AudioStreamRAM, AudioStream)
 
 	enum {
 		FP_BITS = 16, //fixed point used for resampling
 		FP_LEN = (1 << FP_BITS),
 		FP_MASK = FP_LEN - 1,
-        SAFE_FRAMES = 8192 // empty frames reserved
+		SAFE_FRAMES = 8192 // empty frames reserved
 	};
 
 private:
-    friend class AudioStreamPlaybackRAM;
-    bool valid;
-    AudioFrame *data;
-    uint32_t nframes;
-    uint32_t capacity;
-    float length;
-    int mix_rate;
-    // void _premix(int16_t *pcm_data, float position);
-    int _decode_vorbis(String filename);
-    int _resample_from(int source_rate);
-    void update_length();
+	friend class AudioStreamPlaybackRAM;
+	bool valid;
+	AudioFrame *data;
+	uint32_t nframes;
+	uint32_t capacity;
+	float length;
+	int mix_rate;
+	// void _premix(int16_t *pcm_data, float position);
+	int _decode_vorbis(String filename);
+	int _resample_from(int source_rate);
+	void update_length();
 
 public:
-    AudioStreamRAM();
-    ~AudioStreamRAM();
+	AudioStreamRAM();
+	~AudioStreamRAM();
 
-    void load(String path);
-    bool is_valid();
-    // uint32_t get_frame_count();
+	void load(String path);
+	bool is_valid();
+	// uint32_t get_frame_count();
 
-    virtual Ref<AudioStreamPlayback> instance_playback();
-    virtual String get_stream_name() const;
-    virtual float get_length() const;
+	virtual Ref<AudioStreamPlayback> instance_playback();
+	virtual String get_stream_name() const;
+	virtual float get_length() const;
 
 protected:
 	static void _bind_methods();
 };
 
 class AudioStreamPlaybackRAM : public AudioStreamPlayback {
-    GDCLASS(AudioStreamPlaybackRAM, AudioStreamPlayback)
-    friend class AudioStreamRAM;
+	GDCLASS(AudioStreamPlaybackRAM, AudioStreamPlayback)
+	friend class AudioStreamRAM;
 
 private:
-    Ref<AudioStreamRAM> base;
-    bool active;
-    uint32_t position;
+	Ref<AudioStreamRAM> base;
+	bool active;
+	uint32_t position;
 
 public:
-    virtual void start(float p_from_pos = 0.0);
-    virtual void stop();
-    virtual bool is_playing() const;
-    virtual int get_loop_count() const;
-    virtual float get_playback_position() const;
-    virtual void seek(float p_time);
-    virtual void mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames);
-    virtual float get_length() const;
-    AudioStreamPlaybackRAM();
-    ~AudioStreamPlaybackRAM();
+	virtual void start(float p_from_pos = 0.0);
+	virtual void stop();
+	virtual bool is_playing() const;
+	virtual int get_loop_count() const;
+	virtual float get_playback_position() const;
+	virtual void seek(float p_time);
+	virtual void mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames);
+	virtual float get_length() const;
+	AudioStreamPlaybackRAM();
+	~AudioStreamPlaybackRAM();
 };
 
 #endif
